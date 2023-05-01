1abc65199a485a542b3569f073cd9b8b12944e8e
knobbloch <79453359+knobbloch@users.noreply.github.com>
2023-05-01 18:16:54 +0500

    мур

--- Assets/Scripts/slipper.cs
+++ Assets/Scripts/slipper.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class slipper : MonoBehaviour
 {
+
+    int ticks = 0;
+    bool direction;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -11,5 +15,23 @@ public class slipper : MonoBehaviour
             collision.gameObject.GetComponent<Player>().lives = 3;
             _GLOBAL.LoadScene(0);
         }
+    }
+
+    private void Update()
+    {
+        if (ticks <= 180)
+        {
+            Vector3 dir = -transform.right;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, 5f * Time.deltaTime);
+        }
+        else if (ticks <= 360)
+        {
+            Vector3 dir = transform.right;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, 5f * Time.deltaTime);
+        }
+        else
+            ticks = 0;
+
+        ticks++;
     }
 }
