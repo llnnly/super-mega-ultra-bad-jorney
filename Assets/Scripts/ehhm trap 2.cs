cac7c172f1dbc1fdce31e027d2e520ea3f97967b
llnnly <llnnnlly@gmail.com>
2023-05-02 14:13:29 +0500

    camera fixed

--- Assets/Scripts/ehhm trap 2.cs
+++ Assets/Scripts/ehhm trap 2.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class ehhmtrap2 : MonoBehaviour
+public class ehhmtrap2 : TileBase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
